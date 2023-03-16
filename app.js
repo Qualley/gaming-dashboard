ff3a1d01765b70737e1551ed6b06bd021d69aca2
qualley <matt.qualley93@gmail.com>
2023-03-16 13:10:46 -0400

    Update map

--- app.js
+++ app.js
@@ -3,17 +3,14 @@ import { renderCard, renderLikedCard } from './modules/renderCards.js';
 import { slideUp } from './modules/animations.js';
 
 data.map((post) => {
-    // if the post is liked, render the liked card
-    return (post.liked === true ? renderLikedCard(post) : renderCard(post))
+    return (post.liked ? renderLikedCard(post) : renderCard(post))
 })
 
 let cards = Array.from(document.querySelectorAll('.card')),
     title = document.querySelector('.wrapper h1')
 
-// animate the title in
 slideUp(title, 0, 0)
 
 cards.map((card, index) => {
-    // check index to delay animation
     return (index === 0 ? slideUp(card, index, 1.15) : slideUp(card, index, .11))
 })
